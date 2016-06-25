09c60933ed42c70088a01ef20d37036184a155ae
Shoumya Dasgupta <shoumya20@gmail.com>
2016-06-25 18:46:08 +0530

    Updating layout and css for the user form

---
+++ HTML-TEMPLATES/js/md-container-template-1-controller.js
@@ -0,0 +1,50 @@
+function UserFormCtrl($mdDialog) {
+    var formctrl = this;
+    formctrl.user = {
+        title: 'Developer',
+        email: 'ipsum@lorem.com',
+        firstName: '',
+        lastName: '',
+        company: 'Google',
+        address: '1600 Amphitheatre Pkwy',
+        city: 'Mountain View',
+        state: 'CA',
+        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+        postalCode: '94043'
+    };
+    formctrl.showAlert = function (ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        // Modal dialogs should fully cover application
+        // to prevent interaction outside of dialog
+        $mdDialog.show(
+            $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('User Form Submission Confirmation')
+                .textContent('Do You Want to Submit the User Form')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Yes')
+                .targetEvent(ev)
+        );
+    };
+    formctrl.showConfirm = function (ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Form Submission Confirmation')
+            .textContent('Do You Want to Submit the User Form')
+            .ariaLabel('Lucky day')
+            .targetEvent(ev)
+            .ok('Yes')
+            .cancel('No');
+        $mdDialog.show(confirm).then(function () {
+            formctrl.status = 'Form Submitted';
+        }, function () {
+            formctrl.status = 'Form Not Submitted';
+        });
+    };
+    formctrl.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+        'WY').split(' ').map(function (state) {
+            return { abbrev: state };
+        });
+};
